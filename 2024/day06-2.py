8c94d0144f95633a3b20b37d206ebed4dd7b3e5b
pstet <123316024+pcs03@users.noreply.github.com>
2024-12-06 21:09:32 +0100

    Improve runtime of part 2 day 6 2024

--- 2024/day06-2.py
+++ 2024/day06-2.py
@@ -11,12 +11,10 @@ def find_guard() -> tuple[int, int]:
 directions = ((-1, 0), (0, 1), (1, 0), (0, -1))
 starting_position = find_guard()
 
-def trace_path(limit: int = 10000):
+def get_path():
     visited: set[tuple[int, int]] = set()
-    position = starting_position
+    position = find_guard()
     direction = 0
-
-    steps = 0
 
     while True:
         visited.add(position)
@@ -35,32 +33,52 @@ def trace_path(limit: int = 10000):
             new_position = position[0] + delta[0], position[1] + delta[1]
             new_position_char = grid[new_position[0]][new_position[1]]
 
+        position = new_position 
+
+    return visited
+
+def trace_path(limit: int = 10000):
+    visited: set[tuple[int, int]] = set()
+    visited_direction: set[tuple[int, int, int]] = set()
+    position = starting_position
+    direction = 0
+
+    while (position[0], position[1], direction) not in visited_direction:
+        visited.add(position)
+        visited_direction.add((position[0], position[1], direction))
+        delta = directions[direction]
+
+        new_position = position[0] + delta[0], position[1] + delta[1]
+
+        if new_position[0] < 0 or new_position[0] >= len(grid) or new_position[1] < 0 or new_position[1] >= len(grid[0]):
+            return False
+
+        new_position_char = grid[new_position[0]][new_position[1]]
+
+        while new_position_char == '#':
+            direction = (direction + 1) % 4
+            delta = directions[direction]
+            new_position = position[0] + delta[0], position[1] + delta[1]
+            new_position_char = grid[new_position[0]][new_position[1]]
 
         position = new_position 
-        steps += 1
 
-        if steps > limit:
-            return True
-
-    return False
-
+    return True
 
 sum = 0
-for col in range(len(grid)):
-    for row in range(len(grid)):
-        char = grid[col][row]
-        if char == "#":
-            continue
 
-        if (col, row) == starting_position:
-            continue
+for position in get_path():
+    col, row = position 
+    char = grid[col][row]
 
-        grid[col][row] = "#"
-        result = trace_path()
-        grid[col][row] = "."
+    if (col, row) == starting_position:
+        continue
 
-        if result:
-            sum += 1
+    grid[col][row] = "#"
+    result = trace_path()
+    grid[col][row] = "."
 
+    if result:
+        sum += 1
 
 print(sum)
