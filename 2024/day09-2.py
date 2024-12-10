2e4ef36f5accaa7b87d2e336415eead15766cc15
pstet <123316024+pcs03@users.noreply.github.com>
2024-12-10 21:51:04 +0100

    Create the most horrible, bug-prone code known to man

--- 2024/day09-2.py
+++ 2024/day09-2.py
@@ -1,24 +1,59 @@
 with open("./data/day09.txt") as file:
     line: list[int] = [int(num) for num in file.read().strip()]
 
-blocks: list[tuple[int, int | None]] = []
-numbers_count = 0
-
-print(len(line))
+blocks: list[tuple[int | None, int]] = []
 
 for i, number in enumerate(line):
     if i % 2 == 0:
-        blocks.append((number, int(i / 2)))
+        blocks.append((int(i / 2), number))
     else:
-        blocks.append((number, None))
+        blocks.append((None, number))
 
-print(len(blocks))
+def find_id_idx(id: int) -> int:
+    for i in range(len(blocks) - 1, -1, -1):
+        block = blocks[i]
+        if block[0] == id:
+            return i
 
-# checksum = 0
-#
-# for i, number in enumerate(compressed):
-#     checksum += i * number
-#
-# print(checksum)
+    raise RuntimeError(f"id {id} not found")
+
+
+for id in range(int(len(blocks) / 2), -1, -1):
+    id_idx = find_id_idx(id)
+    print(id_idx)
+    id_block = blocks[id_idx]
+    space = id_block[1]
+
+    for idx, block in enumerate(blocks):
+        if idx >= id_idx:
+            break
+        if block[0] is not None:
+            continue
+
+        if block[1] >= space:
+            remaining_space = block[1] - space
+
+            blocks[id_idx] = (None, space)
+            blocks.pop(idx)
+
+            blocks.insert(idx, (None, remaining_space))
+            blocks.insert(idx, (id, space))
+            break
+
+sum = 0
+idx = 0
+for block in blocks:
+    if block[0] is None:
+        idx += block[1]
+        continue
+
+    for i in range(block[1]):
+        sum += idx * block[0]
+        idx += 1
+
+print(sum)
+
+
+
 
 
