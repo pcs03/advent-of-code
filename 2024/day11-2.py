dec60f21e6554ca986ed2ac088d4450b88ba0bbd
pstet <123316024+pcs03@users.noreply.github.com>
2024-12-11 16:37:16 +0100

    I'm a dynamic programming genius

--- 2024/day11-2.py
+++ 2024/day11-2.py
@@ -1,7 +1,21 @@
 from functools import cache
+import math
 
 with open("./data/day11.txt") as file:
     rocks = list(map(int, file.read().strip().split()))
+
+
+@cache
+def get_rock_length(rock: int) -> int:
+    return math.floor(math.log10(rock)) + 1
+
+
+@cache
+def split_rock(rock: int, rock_length: int) -> tuple[int, int]:
+    a: int = rock // 10 ** (rock_length // 2)
+    b: int = rock % 10 ** (rock_length // 2)
+
+    return a, b
 
 
 @cache
@@ -11,18 +25,20 @@ def blink(rock: int, iterations: int) -> int:
 
     if rock == 0:
         return blink(1, iterations - 1)
-    elif len(str(rock)) % 2 == 0:
-        rock_string = str(rock)
-        rock_length = len(str(rock))
-        return blink(int(rock_string[0 : int(rock_length / 2)]), iterations - 1) + blink(
-            int(rock_string[int(rock_length / 2) : rock_length + 1]), iterations - 1
-        )
-    else:
-        return blink(rock * 2024, iterations - 1)
+
+    rock_length = get_rock_length(rock)
+
+    if rock_length % 2 == 0:
+        a, b = split_rock(rock, rock_length)
+
+        return blink(a, iterations - 1) + blink(b, iterations - 1)
+
+    return blink(rock * 2024, iterations - 1)
+
 
 sum = 0
 for rock in rocks:
-    rock_length = blink(rock, 75)
+    rock_length = blink(rock, 450)
     sum += rock_length
 
 print(sum)
