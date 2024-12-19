39abf14d23911647ca204e3a0dfda4e1d7c303e8
pstet <123316024+pcs03@users.noreply.github.com>
2024-12-19 16:52:29 +0100

    Fix day 19 part 2 2024

--- 2024/day19-2.py
+++ 2024/day19-2.py
@@ -8,27 +8,27 @@ with open("./data/day19.txt") as file:
 max_length = max(len(string) for string in patterns)
 
 @cache
-def lookup(design: str, pattern: tuple[str, ...]) -> set[tuple[str, ...]]:
+def lookup(design: str) -> int:
     if len(design) == 0:
-        return {pattern}
+        raise ValueError("design cannot be empty")
 
-    results = set()
-    for i in range(1, max_length + 1):
-        substring = design[:i]
+    total = 0
+    for length in range(1, min(max_length + 1, len(design) + 1)):
+        substring = design[:length]
+        rest = design[length:]
+
         if substring in patterns:
-            results.update(lookup(design[i:], tuple([*pattern, substring])))
+            if len(rest) == 0:
+                total += 1
+            else:
+                total += lookup(rest)
 
-    return results
+    return total
 
 
 sum = 0
 for design in designs:
-    print(design)
-    match = lookup(design, ())
-    if match:
-        sum += len(match)
-    print(match)
-    print()
-
+    number_matches = lookup(design)
+    sum += number_matches
+    
 print(sum)
-print(len(designs))
